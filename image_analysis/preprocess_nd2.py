6226726f0aaf20cefdab227e4f97f67b68886256
kkappel1 <kkappel@broadinstitute.org>
2023-03-30 10:34:44 -0400

    add option to resize files during nd2 to tif conversion for 10x phenotype images

--- image_analysis/preprocess_nd2.py
+++ image_analysis/preprocess_nd2.py
@@ -6,6 +6,7 @@ import ops.firesnake
 from ops.firesnake import Snake
 import os
 import glob
+import skimage
 
 # need to use pims_nd2 because my image has more than 7 channels (!!)
 # for some reason nd2reader only reads in the first 7 channels
@@ -22,6 +23,39 @@ def GFP_nd2_to_tif_rotate_180_and_align(input_filename, output_dir, ch_dapi, ch_
             print( "Site {} for {} saved in {}.".format(site,input_filename.split('/')[-1],output_filename.split('/')[-1]))
             image = image.astype('uint16')
             image = image[[ch_dapi, ch_gfp],:]
+            image = np.rot90( image, k=2, axes=(1,2))
+
+            # align the image to cycle 1 before saving 
+            #cycle1_f = f'plate_10_tiffs_rotated/well_B2/cycle_1/WellB2_ChannelKK_SBS_DAPI,KK_SBS_Cy3,KK_SBS_A594,KK_SBS_Cy5,KK_SBS_Cy7,SBS_Cy7_s4,SBS_Cy5_s1,SBS_Cy5_s4_Seq0000-tile{site}.tif'
+            align_c1_files = glob.glob( f'{base_align_c1_dir}/*-tile{site}.tif')
+            #print( f'{base_align_c1_dir}/*-tile{site}.tif' )
+            if len( align_c1_files ) != 1:
+                print( "probem with align c1 files", align_c1_files )
+                return
+            align_c1_file = align_c1_files[0]
+
+            cycle1_data = read( align_c1_file )
+            cycle1_2chan_data = cycle1_data[0:2]
+            data_1andgfp = np.array( [cycle1_2chan_data, image] )
+            aligned_1and_gfp = Snake._align_SBS( data_1andgfp, method='DAPI' )
+            aligned_gfp_only = aligned_1and_gfp[1,:]
+            save( output_filename, aligned_gfp_only )
+
+def GFP_nd2_to_tif_rotate_180_and_align_resize(input_filename, output_dir, ch_dapi, ch_gfp, base_align_c1_dir ):
+    with ND2_Reader(input_filename) as images:
+        images.iter_axes = 'm'
+        images.bundle_axes = 'cyx'
+
+        # printing the metadata causes an error with pims_nd2
+        #print( images.metadata )
+        for site, image in enumerate(images):
+            #output_filename = f'test_dapi_gfp_tifs/test_dapi_gfp-tile{site}.tif'
+            output_filename = output_dir + '/' + input_filename.split('/')[-1].replace('.nd2','-tile' + str(site) + '.tif' )
+            print( "Site {} for {} saved in {}.".format(site,input_filename.split('/')[-1],output_filename.split('/')[-1]))
+            image = image.astype('uint16')
+            image = image[[ch_dapi, ch_gfp],:]
+            image = skimage.measure.block_reduce( image, block_size=(1,2,2), func=np.mean)
+            image = np.round( image).astype( np.uint16)
             image = np.rot90( image, k=2, axes=(1,2))
 
             # align the image to cycle 1 before saving 
