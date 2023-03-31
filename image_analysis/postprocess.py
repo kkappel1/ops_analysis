c31a48d541302c83e02acd11e37a8c06d5809c4d
kkappel1 <kkappel@broadinstitute.org>
2023-03-31 12:55:17 -0400

    add parallel version of removing duplicate cells

--- image_analysis/postprocess.py
+++ image_analysis/postprocess.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import multiprocessing
 
 def remove_duplicates( df_well ):
     df_well_no_duplicates = pd.DataFrame()
@@ -7,7 +8,7 @@ def remove_duplicates( df_well ):
     num_double = 0
     num_single = 0
     for tile_10x in list(set(df_well['tile_10x'])):
-        print( tile_10x )
+        print( tile_10x, flush=True )
         for cell in df_well[df_well['tile_10x']==tile_10x]['cell']:
             if (df_well_no_duplicates.shape[0] > 0) and (cell in df_well_no_duplicates[df_well_no_duplicates['tile_10x']==tile_10x]['cell'].values):
                 continue
@@ -23,11 +24,44 @@ def remove_duplicates( df_well ):
     print( "Num single", num_single )
     return df_well_no_duplicates
 
+def remove_duplicates_tile( df_well_tile ):
+    df_well_tile_no_duplicates = pd.DataFrame()
+
+    print( "removing duplicates for tile", df_well_tile['tile_10x'].tolist()[0], flush=True )
+
+    for cell in df_well_tile['cell']:
+        if (df_well_tile_no_duplicates.shape[0] > 0) and (cell in df_well_tile_no_duplicates['cell'].values):
+            continue
+        else:
+            cell_df = df_well_tile[(df_well_tile['cell']==cell)].head(1)
+            df_well_tile_no_duplicates = df_well_tile_no_duplicates.append( cell_df )
+
+    return df_well_tile_no_duplicates
+
+def remove_duplicates_parallel( df_well, num_proc ):
+    tile_list = list( set( df_well['tile_10x'] ) )
+
+    with multiprocessing.Pool(processes=num_proc) as pool:
+        filtered_results = pool.map( remove_duplicates_tile, [df_well[df_well['tile_10x']==tile_10x] for tile_10x in tile_list] )
+
+        # combine all the results:
+        df_well_no_duplicates = pd.DataFrame()
+        for df_well_tile in filtered_results:
+            df_well_no_duplicates = df_well_no_duplicates.append( df_well_tile )
+    return df_well_no_duplicates
 
 def read_write_remove_duplicates( df_file ):
 
     df = pd.read_csv( df_file )
     df_no_duplicates = remove_duplicates( df )
+    df_no_duplicates.to_csv( df_file.replace('.csv', '_no_duplicates.csv' ) )
+
+    return
+
+def read_write_remove_duplicates_parallel( df_file, num_proc ):
+
+    df = pd.read_csv( df_file )
+    df_no_duplicates = remove_duplicates_parallel( df, num_proc)
     df_no_duplicates.to_csv( df_file.replace('.csv', '_no_duplicates.csv' ) )
 
     return
@@ -48,3 +82,15 @@ def read_write_remove_duplicates_filter_bad_barcodes( df_file, bad_barcodes ):
     df_no_duplicates_filtered.to_csv( df_file.replace('.csv', '_no_duplicates.csv' ) )
 
     return
+
+def read_write_remove_duplicates_filter_bad_barcodes_parallel( df_file, bad_barcodes, num_proc ):
+
+    df = pd.read_csv( df_file )
+    df_no_duplicates = remove_duplicates_parallel( df, num_proc )
+    df_no_duplicates_filtered = filter_bad_barcodes( df_no_duplicates, bad_barcodes )
+    df_no_duplicates_filtered.to_csv( df_file.replace('.csv', '_no_duplicates.csv' ) )
+
+    return
+
+if __name__ == '__main__':
+    multiprocessing.freeze_support()
--- image_analysis/postprocess_well.py
+++ image_analysis/postprocess_well.py
@@ -2,14 +2,29 @@ import image_analysis
 from image_analysis.postprocess import *
 import pandas as pd
 import argparse
+import datetime
+import os
 
-def postprocess_well( data_file, bad_barcode_file ):
+def postprocess_well( data_file, bad_barcode_file, num_proc, no_overwrite ):
+    if no_overwrite:
+        output_fname = data_file.replace('.csv', '_no_duplicates.csv' )
+        if os.path.exists( output_fname ):
+            return
 
+    begin_time = datetime.datetime.now()
     if bad_barcode_file != '':
         bad_barcodes = pd.read_csv( bad_barcode_file, header=None)[0].to_list()
-        read_write_remove_duplicates_filter_bad_barcodes( data_file, bad_barcodes )
+        if num_proc > 1:
+            read_write_remove_duplicates_filter_bad_barcodes_parallel( data_file, bad_barcodes, num_proc )
+        else:
+            read_write_remove_duplicates_filter_bad_barcodes( data_file, bad_barcodes )
     else:
-        read_write_remove_duplicates( data_file )
+        if num_proc > 1:
+            read_write_remove_duplicates_parallel( data_file, num_proc )
+        else:
+            read_write_remove_duplicates( data_file )
+    end_time = datetime.datetime.now()
+    print("Time postprocessing:", end_time - begin_time, flush=True )
 
     return 
 
@@ -18,6 +33,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser( description="remove duplicate cells and bad barcodes" )
     parser.add_argument( '-data_file', type=str, default="", help='csv file with the data for the well' )
     parser.add_argument( '-bad_barcode_file', type=str, default="", help='file that contains the bad barcodes' )
+    parser.add_argument( '-num_proc', type=int, default=1, help='number of processors to use' )
+    parser.add_argument( '-no_overwrite', default=False, action='store_true', 
+            help='Dont overwrite existing nuclei segmentation files' )
 
     args = parser.parse_args()
-    postprocess_well( args.data_file, args.bad_barcode_file )
+    postprocess_well( args.data_file, args.bad_barcode_file, args.num_proc, args.no_overwrite )
