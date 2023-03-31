b7f712241acdd956f9ed4891d784c666cd802e28
kkappel1 <kkappel@broadinstitute.org>
2023-03-31 13:36:09 -0400

    filter cell-cell matches in postprocessing

---
+++ image_analysis/filter_matches_well.py
@@ -0,0 +1,41 @@
+import image_analysis
+from image_analysis.postprocess import *
+import pandas as pd
+import argparse
+import datetime
+import os
+
+def filter_matches_well( data_file, no_overwrite ):
+
+    begin_time = datetime.datetime.now()
+    full_data = pd.read_csv( data_file )
+    plot_save_name_intensity = os.path.dirname( data_file ) + '/plot_filter_matches_intensity_' + data_file.split('/')[-1] + '.png'
+    plot_save_name_glcm = os.path.dirname( data_file ) + '/plot_filter_matches_glcm_' + data_file.split('/')[-1] + '.png'
+
+    full_data_filt_int = filter_matches_intensity_range( full_data, residual_scalar_range=[0.01, 0.05, 0.1, 0.15, 0.5, 1.0], 
+                                                         alpha=0.1, residual_offset=100, show_plot=False,
+                                                         plot_save_name=plot_save_name_intensity)
+    
+    full_data_filt_int_glcm = filter_matches_glcm_range( full_data_filt_int, residual_scalar_range=[0.01, 0.05, 0.1, 0.15, 0.5, 1.0], 
+                                                                      glcm_residual_offset=1, show_plot=False,
+                                                                      plot_save_name=plot_save_name_glcm)
+
+    save_data_file = data_file.replace( '.csv', '_filter_matches.csv' )
+    full_data_filt_int_glcm.to_csv( save_data_file, index=False )
+
+
+
+    end_time = datetime.datetime.now()
+    print("Time filtering matches:", end_time - begin_time, flush=True )
+
+    return 
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser( description="remove duplicate cells and bad barcodes" )
+    parser.add_argument( '-data_file', type=str, default="", help='csv file with the data for the well' )
+    parser.add_argument( '-no_overwrite', default=False, action='store_true', 
+            help='Dont overwrite existing nuclei segmentation files' )
+
+    args = parser.parse_args()
+    filter_matches_well( args.data_file, args.no_overwrite )
--- image_analysis/postprocess.py
+++ image_analysis/postprocess.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import multiprocessing
+import matplotlib.pyplot as plt
+from sklearn import linear_model
 
 def remove_duplicates( df_well ):
     df_well_no_duplicates = pd.DataFrame()
@@ -91,6 +93,105 @@ def read_write_remove_duplicates_filter_bad_barcodes_parallel( df_file, bad_barc
     df_no_duplicates_filtered.to_csv( df_file.replace('.csv', '_no_duplicates.csv' ) )
 
     return
+
+def filter_matches_glcm(full_data, residual_scalar=0.08, glcm_cutoff_fitting_10x=15000,
+                            show_plot=True, plot_save_name='', glcm_residual_offset=1):
+    ransac = linear_model.RANSACRegressor(max_trials=500, min_samples=0.6)
+    x_below_cutoff_intensity = full_data['match_glcm_dissim_10x'] < glcm_cutoff_fitting_10x
+    X_all_intensities_10x = full_data['match_glcm_dissim_10x'][x_below_cutoff_intensity][:, np.newaxis]
+    all_intensities_40x = full_data['match_glcm_dissim_40x'][x_below_cutoff_intensity]
+
+    ransac.fit( X_all_intensities_10x, all_intensities_40x )
+    line_X = np.arange( X_all_intensities_10x.min(), X_all_intensities_10x.max())[:, np.newaxis]
+    line_y = ransac.predict( line_X )
+    
+    residuals = ransac.predict(full_data['match_glcm_dissim_10x'][:, np.newaxis]) - full_data['match_glcm_dissim_40x']
+
+
+    residuals_binary = np.abs(residuals ) < (full_data['match_glcm_dissim_10x']*residual_scalar)+glcm_residual_offset
+    line_y_residual_above = line_y + line_X[:,0]*residual_scalar+glcm_residual_offset
+    line_y_residual_below = line_y - line_X[:,0]*residual_scalar-glcm_residual_offset
+
+    plt.scatter( full_data['match_glcm_dissim_10x'], full_data['match_glcm_dissim_40x'],
+               alpha=0.1, c=residuals_binary)
+    plt.plot( line_X, line_y, color='red' )
+    plt.plot( line_X, line_y_residual_above, color='cyan' )
+    plt.plot( line_X, line_y_residual_below, color='cyan' )
+    
+    if plot_save_name != '':
+        plt.savefig( plot_save_name )
+
+    if show_plot:
+        plt.show()
+    plt.clf()
+    
+    fraction_filtered = np.sum(~residuals_binary)/len(residuals_binary)
+    print( "Fraction filtered", fraction_filtered )
+    
+    return full_data[residuals_binary], fraction_filtered
+
+def filter_matches_intensity(full_data, residual_scalar=0.08, intensity_cutoff_fitting_10x=15000,
+                            show_plot=True, plot_save_name='', alpha=0.1, residual_offset=0):
+    ransac = linear_model.RANSACRegressor(max_trials=500, min_samples=0.6)
+    x_below_cutoff_intensity = full_data['match_intensity_10x'] < intensity_cutoff_fitting_10x
+    X_all_intensities_10x = full_data['match_intensity_10x'][x_below_cutoff_intensity][:, np.newaxis]
+    all_intensities_40x = full_data['match_intensity_40x'][x_below_cutoff_intensity]
+
+    ransac.fit( X_all_intensities_10x, all_intensities_40x )
+    line_X = np.arange( X_all_intensities_10x.min(), 2*X_all_intensities_10x.max())[:, np.newaxis]
+    line_y = ransac.predict( line_X )
+    
+    residuals = ransac.predict(full_data['match_intensity_10x'][:, np.newaxis]) - full_data['match_intensity_40x']
+
+
+    residuals_binary = np.abs(residuals ) < (full_data['match_intensity_10x']*residual_scalar)+residual_offset
+    line_y_residual_above = line_y + line_X[:,0]*residual_scalar+residual_offset
+    line_y_residual_below = line_y - line_X[:,0]*residual_scalar-residual_offset
+
+    plt.scatter( full_data['match_intensity_10x'], full_data['match_intensity_40x'],
+               alpha=alpha, c=residuals_binary)
+    plt.plot( line_X, line_y, color='red' )
+    plt.plot( line_X, line_y_residual_above, color='cyan' )
+    plt.plot( line_X, line_y_residual_below, color='cyan' )
+
+    if plot_save_name != '':
+        plt.savefig( plot_save_name )
+    
+    if show_plot:
+        plt.show()
+    plt.clf()
+    
+    fraction_filtered = np.sum(~residuals_binary)/len(residuals_binary)
+    print( "Fraction filtered", fraction_filtered, flush=True )
+    
+    return full_data[residuals_binary], fraction_filtered
+    
+def filter_matches_intensity_range(full_data, residual_scalar_range = [0.01, 0.05, 0.1, 0.15, 0.5, 1.0], 
+                                   intensity_cutoff_fitting_10x=15000,
+                            show_plot=True, plot_save_name='', alpha=0.1, residual_offset=0, fraction_filter_data_threshold=0.01):
+    for residual_scalar in sorted(residual_scalar_range):
+        filtered_data, fraction_filtered  = filter_matches_intensity( full_data, 
+                                                                     residual_scalar, 
+                                                                     intensity_cutoff_fitting_10x, show_plot=show_plot,
+                                                                    plot_save_name=plot_save_name, alpha=alpha, 
+                                                                    residual_offset=residual_offset)
+        if fraction_filtered < fraction_filter_data_threshold:
+            print( "FINAL INTENSITY selected residual_scalar", residual_scalar, flush=True )
+            print( "FINAL INTENSITY fraction data filtered", fraction_filtered, flush=True )
+            return filtered_data
+        
+def filter_matches_glcm_range(full_data, residual_scalar_range=[0.01, 0.05, 0.1, 0.15, 0.5, 1.0], glcm_cutoff_fitting_10x=15000,
+                            show_plot=True, plot_save_name='', glcm_residual_offset=1, fraction_filter_data_threshold=0.005):
+    for residual_scalar in sorted( residual_scalar_range ):
+        filtered_data, fraction_filtered = filter_matches_glcm(full_data, residual_scalar=residual_scalar, 
+                                                               glcm_cutoff_fitting_10x=glcm_cutoff_fitting_10x,
+                                                            show_plot=show_plot, plot_save_name=plot_save_name, 
+                                                               glcm_residual_offset=glcm_residual_offset)
+        if fraction_filtered < fraction_filter_data_threshold:
+            print( "FINAL GLCM selected residual_scalar", residual_scalar, flush=True )
+            print( "FINAL GLCM fraction data filtered", fraction_filtered, flush=True ) 
+            return filtered_data
+
 
 if __name__ == '__main__':
     multiprocessing.freeze_support()
