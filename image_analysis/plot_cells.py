889f9a1ac655c0465d1f93f7df7f8a597b8b7e7e
kkappel1 <kkappel@broadinstitute.org>
2023-04-21 10:40:52 -0400

    add plotting by sublib

--- image_analysis/plot_cells.py
+++ image_analysis/plot_cells.py
@@ -111,3 +111,68 @@ def plot_all_cells_barcode( df_data, barcode, plot_save_name, vmax=3000. ):
     end_time = datetime.datetime.now()
     
     print("Time plotting:", end_time - begin_time )
+
+def plot_example_cells_sublib_sorted( df_data_all, barcodes, sublibs_for_bcs, plot_save_name, NUM_IMGS=30, 
+        mean_intensity_cutoff=150., vmax=3000., descriptions=[] ):
+    ################## plot some example cells:
+    begin_time = datetime.datetime.now()
+
+    # can I plot a bunch of cells for a given barcode?
+    print( "making axes" )
+    print( len( barcodes ) )
+    #fig, ax = plt.subplots(len(barcodes) , NUM_IMGS)
+    fig, ax = plt.subplots(len(barcodes) , NUM_IMGS, figsize=(NUM_IMGS,len(barcodes)))
+    print( "done making axes" )
+    for num, barcode in enumerate(barcodes):
+        print( "getting sublib" )
+        df_data = df_data_all[df_data_all['sublibrary']==sublibs_for_bcs[num]]
+        print( "getting bc etc" )
+        cells_with_barcode = df_data[(df_data['cell_barcode_0']==barcode) &
+                                        (df_data['cell_barcode_1'].isnull()) & 
+                                        (df_data['mean_GFP_intensity_GFP'] > mean_intensity_cutoff)]
+        print( "getting num sample" )
+        num_sample = min( NUM_IMGS, len( cells_with_barcode ) )
+        print( "sorting" )
+        cells_with_barcode_subset_sorted = cells_with_barcode.sample( n=num_sample ).sort_values('mean_GFP_intensity_GFP')
+        img_num = 0
+        print( "plotting", barcode )
+        for index, cell in cells_with_barcode_subset_sorted.iterrows():
+            if img_num >(NUM_IMGS-1): break
+            # get the gfp image
+            # get the image mask
+            cell_image_gfp_file = cell['cell_img_gfp_file']
+            cell_image_mask_file = cell['cell_img_mask_file']
+            cell_image_gfp = read( cell_image_gfp_file )
+            cell_image_mask = np.load( cell_image_mask_file )
+            masked_cell_image_gfp = cell_image_mask * cell_image_gfp
+            masked_cell_pixels_gfp = cell_image_gfp[ cell_image_mask ]
+
+            ax[num][img_num].set_axis_off()
+            pad_x_size = max(0, 185 - np.shape(masked_cell_image_gfp)[0])
+            pad_y_size = max(0, 185 - np.shape(masked_cell_image_gfp)[1])
+            padded_image = np.pad( masked_cell_image_gfp, [(int(pad_x_size/2),int(pad_x_size/2)), 
+                                            (int(pad_y_size/2),int(pad_y_size/2))],
+                                            'constant', constant_values=0)
+
+            ax[num][img_num].imshow( padded_image, cmap='gray', vmin=0, vmax=vmax )
+            if len(descriptions) > 0:
+                ax[num][img_num].set_title(descriptions[num],fontsize=2,color='blue')
+                if img_num == 0:
+                    ax[num][img_num].set_ylabel(descriptions[num],fontsize=4,color='blue', rotation=0)
+            else:
+                ax[num][img_num].set_title(barcode,fontsize=4,color='blue')
+            ax[num][img_num].text(0.1,0.9,cell['num_condensates_GFP'],fontsize=4,color='blue', transform=ax[num][img_num].transAxes)
+            ax[num][img_num].text(0.1,0.5,f"{cell['glcm_dissim']:.2f}",fontsize=4,color='blue' ,transform=ax[num][img_num].transAxes)
+            ax[num][img_num].text(0.1,0.3,f"{cell['mean_GFP_intensity_GFP']:.2f}",fontsize=4,color='blue' ,transform=ax[num][img_num].transAxes)
+            ax[num][img_num].text(0.1,0.1,f"{cell['frac_gfp_in_cond_GFP']:.2f}",fontsize=4,color='blue' ,transform=ax[num][img_num].transAxes)
+            img_num += 1
+
+    # turn all axes off
+    for i in range(len(barcodes)):
+        for j in range(NUM_IMGS):
+            ax[i][j].set_axis_off()
+
+    plt.savefig( f'{plot_save_name}', dpi=300 )
+    plt.clf()
+    end_time = datetime.datetime.now()
+    print("Time plotting:", end_time - begin_time )
